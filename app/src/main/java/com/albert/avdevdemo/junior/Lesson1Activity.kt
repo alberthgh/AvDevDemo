59435d9fdb13bbb7630ab0e13832657ad4f6e0ef
Albert <313424545@qq.com>
2023-09-21 21:15:27 +0800

    Initial commit

---
+++ app/src/main/java/com/albert/avdevdemo/junior/Lesson1Activity.kt
@@ -0,0 +1,13 @@
+package com.albert.avdevdemo.junior
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.albert.avdevdemo.R
+
+//通过三种方式绘制图片
+class Lesson1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
