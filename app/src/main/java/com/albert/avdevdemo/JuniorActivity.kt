b65aa317326653642d97f17d6ed70e5c74d04319
Albert <313424545@qq.com>
2023-10-13 16:03:20 +0800

    初级：第二课

--- app/src/main/java/com/albert/avdevdemo/JuniorActivity.kt
+++ app/src/main/java/com/albert/avdevdemo/JuniorActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.albert.avdevdemo.databinding.ActivityJuniorBinding
 import com.albert.avdevdemo.junior.Lesson1Activity
+import com.albert.avdevdemo.junior.Lesson2Activity
 
 class JuniorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,9 @@ class JuniorActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.lesson1.setOnClickListener {
             startActivity(Intent(this, Lesson1Activity::class.java))
+        }
+        binding.lesson2.setOnClickListener {
+            startActivity(Intent(this, Lesson2Activity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/albert/avdevdemo/junior/Lesson2Activity.kt
@@ -0,0 +1,186 @@
+package com.albert.avdevdemo.junior
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnPreparedListener
+import android.media.MediaRecorder
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.albert.avdevdemo.databinding.ActivityJuniorLesson2Binding
+import com.albert.avdevdemo.misc.FileUtil
+import com.albert.avdevdemo.misc.PcmToWavUtil
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+
+//使用 AudioRecord 采集音频PCM并保存到文件
+class Lesson2Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityJuniorLesson2Binding
+
+    //核心AudioRecord对象
+    private lateinit var audioRecord: AudioRecord
+    private var bufferSize = 1024
+    private lateinit var buffer: ByteArray
+
+    @Volatile
+    private var isRecording = false
+    private var wav: File? = null
+    //播放
+
+    private lateinit var mediaPlayer: MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityJuniorLesson2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        initAudioRecord()
+        binding.btnRecord.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    111
+                )
+            } else {
+                startRecord()
+            }
+        }
+        binding.btnStopRecord.setOnClickListener {
+            isRecording = false
+        }
+        binding.btnPlayRecord.setOnClickListener {
+            wav?.let {
+                playMedia(Uri.fromFile(it))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111) {
+            startRecord()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startRecord() {
+        Thread {
+            val p1 = AudioFormat.SAMPLE_RATE_UNSPECIFIED
+            val p2 = AudioFormat.CHANNEL_IN_STEREO
+            val p3 = AudioFormat.ENCODING_PCM_16BIT
+//            bufferSize = AudioRecord.getMinBufferSize(
+//                p1,
+//                p2,
+//                p3
+//            )//参数的东西，文档默认吧
+//            if (bufferSize<0){
+//                Log.d("record","bufferSize < 0")
+//                return@Thread
+//            }
+            buffer = ByteArray(bufferSize)
+            audioRecord = AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                p1,
+                p2,
+                p3, bufferSize
+            )
+            audioRecord.startRecording()
+            isRecording = true
+            //创建一个数据流！！
+            //一边从AudioRecord中读取声音数据 到初始化的buffer，一边将buffer中数据 导入到数据流
+            val subPath = "/" + "录音测试"
+            FileUtil.createFileOnDefaultPath(this, subPath)
+            val filePath = FileUtil.getDefaultPath(this) + subPath
+            var fileOs: FileOutputStream? = null
+            try {
+                fileOs = FileOutputStream(filePath)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
+
+            if (fileOs == null) return@Thread
+            var readResult = 0
+            while (isRecording) {
+                readResult = audioRecord.read(buffer, 0, bufferSize)//这一步：从系统层读取数据
+                Log.d("record", "readResult_ " + readResult)
+                //如果从系统层读取音频数据没有出现错误，就可以根据 业务场景，对数据进行后续处理了。
+                // 这里是：将数据写入到文件
+                if (AudioRecord.ERROR_INVALID_OPERATION != readResult) {
+                    try {
+                        fileOs.write(buffer)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            //暂停录制
+            audioRecord.stop()
+            //关闭资源
+            try {
+                fileOs.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            //加音频的 文件头
+            val wavPath = filePath + ".wav"
+            PcmToWavUtil(p1, p2, bufferSize).pcmToWav(filePath, wavPath)
+            wav = File(wavPath)
+        }.start()
+    }
+
+
+    private fun initAudioRecord() {
+    }
+
+    fun playMedia(uri: Uri) {
+        mediaPlayer = MediaPlayer.create(this, uri).apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            try {
+                //change with setDataSource(Context,Uri);
+                mediaPlayer.setDataSource(this@Lesson2Activity, uri)
+                mediaPlayer.prepareAsync()
+                mediaPlayer.setOnPreparedListener(OnPreparedListener { //mp.start();
+                    mediaPlayer.start()
+                })
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+            } catch (e: IllegalStateException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        audioRecord.release()
+    }
+}
